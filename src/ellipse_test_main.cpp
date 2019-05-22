8fd82e8cbd060a68f011c8f0ad2c908ec34a2412
Albert Palomer Vila <albert.palo@gmail.com>
2019-05-22 09:21:21 +0200

    Changes to remove the cmd window in windows

--- src/ellipse_test_main.cpp
+++ src/ellipse_test_main.cpp
@@ -1,6 +1,9 @@
 #include <QApplication>
 #include "ellipsetest.h"
 
+#ifdef _WIN32
+  #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+#endif
 int main(int argc, char** argv)
 {
   QApplication app(argc, argv);
